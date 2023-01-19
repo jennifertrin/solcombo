56d70bba359dfbfeb5a9611c5e436dfa69fe7e61
Jennifer K. Tran <38402540+jennifertrin@users.noreply.github.com>
2023-01-18 16:59:22 -0800

    add warning header (#2)

---
+++ solcombo/src/components/layout/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex w-screen bg-purple-300">
+      <div className="font-body font-bold tracking-wide mx-auto px-4 py-2 h-2/12 text-white">
+        BE AWARE: All Sol-combo links start with www.sol-combo.com/. Do not
+        click on links that claim to be from Sol-Combo with a different domain!{" "}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
