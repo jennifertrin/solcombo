1dfa91e4367f96a63b1cd6144d988f587e8d11a2
jennifertrin <38402540+jennifertrin@users.noreply.github.com>
2023-01-18 23:30:17 -0800

    setup dialect

--- solcombo/src/pages/_app.tsx
+++ solcombo/src/pages/_app.tsx
@@ -3,6 +3,12 @@ import type { AppProps } from "next/app";
 import { ConnectionProvider } from "@solana/wallet-adapter-react";
 import dynamic from "next/dynamic";
 import Layout from "@/components/layout/Layout";
+import '@dialectlabs/react-ui/index.css';
+import { 
+  DialectUiManagementProvider, 
+  DialectThemeProvider, 
+  DialectNoBlockchainSdk 
+} from '@dialectlabs/react-ui';
 
 export default function App({ Component, pageProps }: AppProps) {
   const endpoint = "https://rpc.ankr.com/solana";
@@ -14,12 +20,30 @@ export default function App({ Component, pageProps }: AppProps) {
     }
   );
 
+  const SdkProvider = ({children} : React.PropsWithChildren<{}>) => {
+    return <DialectNoBlockchainSdk>{children}</DialectNoBlockchainSdk>;
+  }
+  
+  const DialectProviders = ({children} : React.PropsWithChildren<{}>) => {
+    return (
+      <SdkProvider>
+        <DialectThemeProvider>
+          <DialectUiManagementProvider>
+            {children}
+          </DialectUiManagementProvider>
+        </DialectThemeProvider>
+      </SdkProvider>
+    );
+  }
+
   return (
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider>
+        <DialectProviders>
         <Layout>
           <Component {...pageProps} />
         </Layout>
+        </DialectProviders>
       </WalletProvider>
     </ConnectionProvider>
   );
