3a223d10a8147bfdf6adad8d08382805a5a5dbd7
Jennifer K. Tran <38402540+jennifertrin@users.noreply.github.com>
2023-01-20 14:43:59 -0800

    Add Instructions (#5)
    * add text instructions
    
    * add instructions on how to play

--- solcombo/src/pages/howitworks.tsx
+++ solcombo/src/pages/howitworks.tsx
@@ -1,6 +1,9 @@
+import ListInstructions from "@/components/ListInstructions";
+
 export default function Instructions() {
   return (
     <div className="flex w-screen h-screen">
+      <ListInstructions />
     </div>
   )
 }
