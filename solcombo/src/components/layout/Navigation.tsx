cb08e9283d41e7d6a4bcc6c8fef2cced56d3aec1
Jennifer K. Tran <38402540+jennifertrin@users.noreply.github.com>
2023-01-22 21:37:41 -0800

    Fix: Do Not Lazy Load Pages to Links (#8)
    * chore: do not lazy load pages to links
    
    * revert changes to _app
    
    * chore: remove unused import
    
    * chore: disable requiring using anchor

--- solcombo/src/components/layout/Navigation.tsx
+++ solcombo/src/components/layout/Navigation.tsx
@@ -18,12 +18,12 @@ const Navigation = () => {
     <ul className="flex flex-col w-1/4 h-1/3 bg-white font-body text-xl tracking-wider my-auto ml-0 md:ml-24">
       {navigationItems.map((item) => (
         <li key={item.label} className="flex-1 mr-2">
-          <Link
+          <a
             className={`text-center ${router.pathname === item.href ? 'font-bold' : 'font-normal'} block rounded py-4 px-4 text-black`}
             href={item.href}
           >
             {item.label}
-          </Link>
+          </a>
         </li>
       ))}
     </ul>
