901bfb4de7c445ef953e111246adce166e5b8583
jennifertrin <38402540+jennifertrin@users.noreply.github.com>
2023-01-22 23:34:51 -0800

    add iframe of flipside dashboard

--- solcombo/src/pages/analytics.tsx
+++ solcombo/src/pages/analytics.tsx
@@ -1,6 +1,7 @@
 export default function Analytics() {
     return (
       <div className="flex w-screen h-screen">
+        <iframe src="https://app.flipsidecrypto.com/dashboard/the-state-of-the-cyber-samurai-collection-d86DrV" name="Flipside iframe" className='w-full h-screen mx-12 mt-6' />
       </div>
     )
   }
