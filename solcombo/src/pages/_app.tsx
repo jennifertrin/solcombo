f7989fef0625b85b0c81e0acb78d45fdee9d439f
jennifertrin <38402540+jennifertrin@users.noreply.github.com>
2023-01-22 21:21:25 -0800

    revert changes to _app

--- solcombo/src/pages/_app.tsx
+++ solcombo/src/pages/_app.tsx
@@ -3,18 +3,14 @@ import type { AppProps } from "next/app";
 import { ConnectionProvider, useWallet } from "@solana/wallet-adapter-react";
 import dynamic from "next/dynamic";
 import Layout from "@/components/layout/Layout";
-import "@dialectlabs/react-ui/index.css";
-import {
-  DialectUiManagementProvider,
-  DialectThemeProvider,
-  DialectNoBlockchainSdk,
-  ConfigProps,
-} from "@dialectlabs/react-ui";
-import {
-  DialectSolanaSdk,
-  DialectSolanaWalletAdapter,
-  SolanaConfigProps,
-} from "@dialectlabs/react-sdk-blockchain-solana";
+import '@dialectlabs/react-ui/index.css';
+import { 
+  DialectUiManagementProvider, 
+  DialectThemeProvider, 
+  DialectNoBlockchainSdk, 
+  ConfigProps
+} from '@dialectlabs/react-ui';
+import { DialectSolanaSdk, DialectSolanaWalletAdapter, SolanaConfigProps } from '@dialectlabs/react-sdk-blockchain-solana';
 import { useEffect, useMemo, useState } from "react";
 import { solanaWalletToDialectWallet } from "@/util/SolanaToDialect";
 
@@ -28,67 +24,57 @@ export default function App({ Component, pageProps }: AppProps) {
     }
   );
 
-  const SdkProvider = ({ children }: React.PropsWithChildren<{}>) => {
+  const SdkProvider = ({children} : React.PropsWithChildren<{}>) => {
     const solanaWallet = useWallet();
 
     const [dialectSolanaWalletAdapter, setDialectSolanaWalletAdapter] =
-      useState<DialectSolanaWalletAdapter | null>(null);
+    useState<DialectSolanaWalletAdapter | null>(null);
+  
+  const dialectConfig: ConfigProps = useMemo(() => ({
+    environment: 'production',
+    dialectCloud: {
+      tokenStore: 'local-storage',
+    }
+  }), []);
 
-    const dialectConfig: ConfigProps = useMemo(
-      () => ({
-        environment: "production",
-        dialectCloud: {
-          tokenStore: "local-storage",
-        },
-      }),
-      []
+  const solanaConfig: SolanaConfigProps = useMemo(() => ({
+    wallet: dialectSolanaWalletAdapter,
+  }), [dialectSolanaWalletAdapter]);
+
+  useEffect(() => {
+    setDialectSolanaWalletAdapter(solanaWalletToDialectWallet(solanaWallet));
+  }, [solanaWallet]);
+    
+  if (dialectSolanaWalletAdapter) {
+    return (
+      <DialectSolanaSdk config={dialectConfig} solanaConfig={solanaConfig}>
+        {children}
+      </DialectSolanaSdk>
     );
+  }
 
-    const solanaConfig: SolanaConfigProps = useMemo(
-      () => ({
-        wallet: dialectSolanaWalletAdapter,
-      }),
-      [dialectSolanaWalletAdapter]
-    );
-
-    useEffect(() => {
-      if (solanaWallet) {
-        const solanaToDialect = solanaWalletToDialectWallet(solanaWallet);
-        setDialectSolanaWalletAdapter(solanaToDialect);
-      }
-    }, [solanaWallet]);
-
-    const provider = useMemo(() => {
-      if (dialectSolanaWalletAdapter) {
-        return (
-          <DialectSolanaSdk config={dialectConfig} solanaConfig={solanaConfig}>
-            {children}
-          </DialectSolanaSdk>
-        );
-      }
-      return <DialectNoBlockchainSdk>{children}</DialectNoBlockchainSdk>;
-    }, [children, dialectConfig, dialectSolanaWalletAdapter, solanaConfig]);
-
-    return provider;
-  };
-
-  const DialectProviders = ({ children }: React.PropsWithChildren<{}>) => {
+  return <DialectNoBlockchainSdk>{children}</DialectNoBlockchainSdk>;
+  }
+  
+  const DialectProviders = ({children} : React.PropsWithChildren<{}>) => {
     return (
       <SdkProvider>
         <DialectThemeProvider>
-          <DialectUiManagementProvider>{children}</DialectUiManagementProvider>
+          <DialectUiManagementProvider>
+            {children}
+          </DialectUiManagementProvider>
         </DialectThemeProvider>
       </SdkProvider>
     );
-  };
+  }
 
   return (
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider>
         <DialectProviders>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
         </DialectProviders>
       </WalletProvider>
     </ConnectionProvider>
